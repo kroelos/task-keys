c3cdc93a8c9b56434200d4f002ce1fe8a5013ff4
Орлов Олег <orlow.tiger@yandex.ru>
2022-12-04 18:04:16 +0300

    React. списки

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,73 @@
 import { IItem } from './index';
+import React, { useState, useEffect } from 'react';
 
 export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+    const [reacted_id, setReactedId] = useState(-20);
+    let inputstring = '';
+    let data_sorted = props.initialData.sort((a, b) => {
+        let fa = a.id,
+            fb = b.id;
+        if (props.sorting == 'ASC') {
+            if (fa < fb) {
+                return -1;
+            }
+            if (fa > fb) {
+                return 1;
+            }
+        }
+        if (props.sorting == 'DESC') {
+            if (fa < fb) {
+                return 1;
+            }
+            if (fa > fb) {
+                return -1;
+            }
+        }
+        return 0;
+    });
+    function handleclick(id: number) {
+        setReactedId(id);
+    }
+
+    function handlekey(e: React.KeyboardEvent, id: number) {
+        if (e.key == 'Enter') {
+            data_sorted.forEach((item) => {
+                if (item.id == id) {
+                    item.name += inputstring;
+                }
+            });
+            setReactedId(-20);
+            inputstring = '';
+        } else {
+            if (e.key == 'Escape') {
+                setReactedId(-20);
+                inputstring = '';
+            } else {
+                inputstring += e.key;
+            }
+        }
+    }
+    let template = (
+        <div>
+            {data_sorted.map((item) => {
+                if (item.id == reacted_id) {
+                    return (
+                        <input
+                            defaultValue={item.name}
+                            key={item.id}
+                            onKeyDown={(event) => handlekey(event, item.id)}
+                        ></input>
+                    );
+                } else {
+                    return (
+                        <li key={item.id} onClick={() => handleclick(item.id)}>
+                            {item.name}
+                        </li>
+                    );
+                }
+            })}
+        </div>
+    );
+
+    return template;
 }
